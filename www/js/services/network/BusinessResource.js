5c75d0f2e3a35383b4ae0c4a325fe131dc3ec8a4
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-07-17 02:06:20 -0300

    Implementando resource para Business

---
+++ www/js/services/network/BusinessResource.js
@@ -0,0 +1,62 @@
+angular.module('goreservas')
+    .factory('UserResource', function($q, RemoteResource) {
+        var Resource = RemoteResource.resource("/api/business/:id/:client/:search/:filter");
+        return {
+            getBusiness: function(id){
+                var business = new Resource();
+                var dfd = $q.defer();
+                business.$get({id: id}).then(
+                    function(response){
+                        if (response.success){
+                            dfd.resolve(response.data);
+                        }
+                        else{
+                            dfd.reject(response.reason);
+                        }
+                    },
+                    function(error){
+                        dfd.reject(error.name);
+                    }
+                );
+                return dfd.promise;
+            },
+            getBestBusiness: function(){
+                var business = new Resource();
+                var dfd = $q.defer();
+                business.$get().then(
+                    function(response){
+                        if (response.success){
+                            dfd.resolve(response.data);
+                        }
+                        else{
+                            dfd.reject(response.reason);
+                        }
+                    },
+                    function(error){
+                        dfd.reject(error.name);
+                    }
+                );
+                return dfd.promise;
+            },
+            searchBusiness: function(client, search, filter){
+                if (search === "")
+                    search = "%all%";
+                var business = new Resource();
+                var dfd = $q.defer();
+                business.$get({client: client, search: search, filter: filter}).then(
+                    function(response){
+                        if (response.success){
+                            dfd.resolve(response.data);
+                        }
+                        else{
+                            dfd.reject(response.reason);
+                        }
+                    },
+                    function(error){
+                        dfd.reject(error.name);
+                    }
+                );
+                return dfd.promise;
+            }
+        }
+    });
