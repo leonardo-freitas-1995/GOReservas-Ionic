cb6de8830b1aaa5ea86e64cd0aa13f74890eb440
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-07-17 02:25:29 -0300

    Implementando ReserveResource

---
+++ www/js/services/network/ReserveResource.js
@@ -0,0 +1,123 @@
+angular.module('goreservas')
+    .factory('ReserveResource', function($q, RemoteResource) {
+        var Resource = RemoteResource.resource("/api/reserve/:id/:business/:date/:client");
+        return {
+            createReserve: function (reserveData) {
+                var newReserve = new Resource(reserveData);
+                var dfd = $q.defer();
+                newReserve.$save().then(
+                    function(response){
+                        if (response.success){
+                            dfd.resolve(response.confirmed);
+                        }
+                        else{
+                            dfd.reject(response.reason);
+                        }
+                    },
+                    function(error){
+                        dfd.reject(error.name);
+                    }
+                );
+
+                return dfd.promise;
+            },
+            rateReserve: function (id, business, rating) {
+                var reserve = new Resource({rating: rating});
+                var dfd = $q.defer();
+                reserve.$save({id: id, business: business}).then(
+                    function(response){
+                        if (response.success){
+                            dfd.resolve();
+                        }
+                        else{
+                            dfd.reject(response.reason);
+                        }
+                    },
+                    function(error){
+                        dfd.reject(error.name);
+                    }
+                );
+
+                return dfd.promise;
+            },
+            getReserves: function(id){
+                var reserve = new Resource();
+                var dfd = $q.defer();
+                reserve.$get({id: id}).then(
+                    function(response){
+                        if (response.success){
+                            dfd.resolve(response.data);
+                        }
+                        else{
+                            dfd.reject(response.reason);
+                        }
+                    },
+                    function(error){
+                        dfd.reject(error.name);
+                    }
+                );
+
+                return dfd.promise;
+            },
+            getLastReserves: function(id){
+                var reserve = new Resource();
+
+                var dfd = $q.defer();
+                reserve.$update({id: id}).then(
+                    function(response){
+                        if (response.success){
+                            dfd.resolve(response.data);
+                        }
+                        else{
+                            dfd.reject(response.reason);
+                        }
+                    },
+                    function(error){
+                        dfd.reject(error.name);
+                    }
+                );
+
+                return dfd.promise;
+            },
+            getOneReserve: function(client, id){
+                var reserve = new Resource();
+
+                var dfd = $q.defer();
+                reserve.$get({client: client, id: id}).then(
+                    function(response){
+                        if (response.success){
+                            dfd.resolve(response.data);
+                        }
+                        else{
+                            dfd.reject(response.reason);
+                        }
+                    },
+                    function(error){
+                        dfd.reject(error.name);
+                    }
+                );
+
+                return dfd.promise;
+            },
+            cancelReserve: function(id, date){
+                var reserve = new Resource();
+
+                var dfd = $q.defer();
+                reserve.$delete({id: id, date: date}).then(
+                    function(response){
+                        if (response.success){
+                            dfd.resolve();
+                        }
+                        else{
+                            dfd.reject(response.reason);
+                        }
+                    },
+                    function(error){
+                        dfd.reject(error.name);
+                    }
+                );
+
+                return dfd.promise;
+            }
+        }
+    });
