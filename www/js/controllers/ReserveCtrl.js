a7b6aa54dd01a782fbfd9fb3e2d56d169bdfb4e9
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-07-18 15:39:29 -0300

    Criando tela da reserva

--- www/js/controllers/ReserveCtrl.js
+++ www/js/controllers/ReserveCtrl.js
@@ -2,8 +2,88 @@
     angular
         .module('goreservas')
         .controller('ReserveCtrl', Controller);
-    Controller.$inject = ['$scope', 'ReserveResource', 'UserIdentity'];
-    function Controller($scope, ReserveResource, UserIdentity) {
+    Controller.$inject = ['$scope', '$sce', '$state', '$ionicPopup', 'ReserveResource', 'UserIdentity', 'Toast'];
+    function Controller($scope, $sce, $state, $ionicPopup, ReserveResource, UserIdentity, Toast) {
 
+        $scope.reserve = null;
+        $scope.rating = 0;
+        $scope.loaded = false;
+
+        $scope.makeMapsLink = function(business){
+            return $sce.trustAsResourceUrl("https://maps.google.com/maps?q=" + business.latitude + "," + business.longitude + "&hl=es;z=14&output=embed");
+        };
+
+        $scope.isBeforeReserve = function(date){
+            var dateA = new Date(date);
+            var dateB = new Date();
+            return (dateB.getTime() < dateA.getTime());
+        };
+
+        $scope.rateReserve = function(){
+            ReserveResource.rateReserve($scope.reserve.id, $scope.reserve.business.id, $scope.rating).then(function(){
+                    Toast.success("Reserva avaliada com sucesso");
+                    $scope.reserve.rated = true;
+                },
+                function(){
+                    Toast.error("Não foi possível completar esta ação");
+                });
+        };
+
+        $scope.cancelReserve = function(){
+            var logoutPopup = $ionicPopup.show({
+                template: '',
+                title: 'Cancelar',
+                subTitle: 'Deseja mesmo cancelar esta reserva?',
+                buttons: [
+                    { text: 'Não' },
+                    {
+                        text: '<b>Sim</b>',
+                        type: 'button-assertive',
+                        onTap: function(e) {
+                            ReserveResource.cancelReserve($scope.reserve.id, new Date($scope.reserve.date).getTime()).then(function(response){
+                                    Toast.success("Reserva cancelada com sucesso.");
+                                    $state.go("dashboard")
+                                },
+                                function(reason){
+                                    if (reason === "ahead of time"){
+                                        Toast.error("Não é possível cancelar uma reserva que já passou de sua data.");
+                                    }
+                                    else{
+                                        Toast.error("Não foi possível completar esta ação");
+                                    }
+                                });
+                        }
+                    }
+                ]
+            });
+        };
+
+        $scope.refresh = function(){
+            refreshReserve();
+        };
+
+        $scope.changeRating = function(rating){
+            $scope.rating = rating;
+        };
+
+        function refreshReserve(){
+            var id = $state.params.id;
+            if (id && id !== ""){
+                ReserveResource.getOneReserve(UserIdentity.getCurrentUser().id, id).then(function(data){
+                        $scope.reserve = data;
+                        if ($scope.reserve.rated)
+                            $scope.rating = $scope.reserve.rating;
+                        $scope.loaded = true;
+                    },
+                    function(){
+                        $scope.loaded = true;
+                    });
+            }
+            else{
+                $scope.loaded = true;
+            }
+        }
+
+        refreshReserve();
     }
 })();
